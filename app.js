e556927e21ac3dde2b3d18949d197ffc7adef383
justinsrd <jsrdeluna@gmail.com>
2019-11-06 09:45:46 -0800

    More stuff I guess

--- app.js
+++ app.js
@@ -1,99 +1,136 @@
 'use strict';
 
-let fs = require('fs');
-let express = require('express');
-let app = express();
-let bodyParser = require('body-parser');
-let morgan = require('morgan');
-let ytdl = require('ytdl-core');
-let ffmpeg = require('fluent-ffmpeg');
-let request = require('request');
-let cheerio = require('cheerio');
-let port = process.env.PORT || 3001;
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const NodeID3 = require('node-id3');
+const ffmpeg = require('fluent-ffmpeg');
+// let command = ffmpeg();
+const cheerio = require('cheerio');
+const request = require('request');
+const ytdl = require('ytdl-core');
+const port = process.env.PORT || 3000;
 
 app.use(express.static('./public'));
-
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.post('/api/single', function(req, res) {
-  ytdl.getInfo(req.body.url, function(err, info) {
-    res.send({title: info.title, url: req.body.url});
-  });
-});
+const audioOutputPath = './output_audio/';
+const videoOutputPath = './output_video/';
+if (!fs.existsSync(audioOutputPath)) {
+	fs.mkdirSync(audioOutputPath);
+}
+if (!fs.existsSync(videoOutputPath)) {
+	fs.mkdirSync(videoOutputPath);
+}
 
-app.post('/api/playlist', function(req, res) {
-  let resultPlaylist = [];
-  let plUrl = req.body.url;
-  request(plUrl, function (error, response, html) {
-    if (!error && response.statusCode == 200) {
-      let $ = cheerio.load(html);
-      let list = $('tbody#pl-load-more-destination').find('tr .pl-video-title a');
-      for (let i = 0; i < list.length; i++) {
-        //resultPlaylist.push(list[i].attribs.href);
-        // let songUrl = list[i].attribs.href;
-        let songUrl = list[i].textContent.trim();
-        ytdl.getInfo(songUrl, function(err, info) {
-          resultPlaylist.push({ title: info.title, url:songUrl });
-          if (resultPlaylist.length === list.length) {
-            res.send(resultPlaylist);
-          }
-        });
-      }
-    } else {
-      console.log(error);
-      console.log(response.statusCode);
-      res.send('Invalid input!')
-    } 
-  });
+app.get('/api/playlist/:url', function(req, res) {
+	const playlistInfo = [];
+	const url = req.params.url;
+	request('https://www.youtube.com/playlist?list=' + url, function(error, response, html) {
+		if (!error && response.statusCode === 200) {
+			const $ = cheerio.load(html);
+			const titles = $('tbody#pl-load-more-destination tr.pl-video');
+			const links = titles.find('a.pl-video-title-link');
+
+			if (titles.length === links.length) {
+				for (let i = 0; i < titles.length; i++) {
+					playlistInfo.push({
+						label: titles[i].attribs['data-title'],
+						link: 'https://youtube.com' + links[i].attribs.href
+					});
+				}
+			}
+			res.json(playlistInfo);
+		} else {
+			res.json({});
+		}
+	});
 });
 
 app.post('/api/download', function(req, res) {
-  let startDate = new Date();
-  let url = req.body.url;
-  let stream = ytdl(url);
-  let audioOutputPath = './output_audio/';
-  let videoOutputPath = './output_video/';
-
-  if (!fs.existsSync(audioOutputPath)) {
-      fs.mkdirSync(audioOutputPath);
-  }
-  if (!fs.existsSync(videoOutputPath)) {
-      fs.mkdirSync(videoOutputPath);
-  }
-
-  ytdl.getInfo(url, function(err, info) {
-    let songTitle = info.title.replace(/[\/]/g, '|');
-    //console.log(info);
-    if(fs.existsSync(audioOutputPath+songTitle+'.mp3')) {
-      res.send('Song already exists!');
-      //res.json(info);
-    } else {
-      //console.log('Downloading ::::: ' + info.title);
-      stream.pipe(fs.createWriteStream(videoOutputPath + songTitle + '.mp4'));
-      stream.on('end', function() {
-        //console.log('Video finished downloading. Conversion to mp3 starting now.....');
-        let proc = new ffmpeg({source: videoOutputPath + songTitle + '.mp4'}).audioBitrate('320');
-        proc.save(audioOutputPath + songTitle + '.mp3');
-        let endDate = new Date();
-        let totalTime = Math.round((endDate - startDate) / 1000);
-        //console.log('Mp3 conversion finished!')
-        res.send('Finished converting to mp3 in ' + totalTime + ' seconds!');
-      });
-    }
-  });
+	downloadPlaylist(req, res);
 });
 
-app.delete('/api/videos', function(req, res) {
-  let url = req.body.url;
-  ytdl.getInfo(url, function(err, info) {
-    let songTitle = info.title.replace(/[\/]/g, '|');
-    fs.unlink('./output_video/' + songTitle + '.mp4');
-    res.send('Song was deleted!');
-  });
+function downloadPlaylist(req, res) {
+	const tracks = req.body.tracks || [];
+	tracks.forEach((track) => {
+        console.log(track.label);
+        console.log(track.link);
+    });
+
+	function download() {
+		const song = tracks.shift();
+		if (song) {
+			const start = new Date();
+			const songTitle = song.label.replace(/[\/]/g, '|');
+			console.log('starting dl for ' + songTitle);
+			if (!fs.existsSync(audioOutputPath + songTitle + '.mp3')) {
+				const stream = ytdl(song.link, {filter: 'audioonly'});
+				stream.pipe(fs.createWriteStream(videoOutputPath + songTitle + '.mp4'));
+				stream.on('end', function() {
+					const command = ffmpeg({source: videoOutputPath + songTitle + '.mp4'}).audioBitrate(320);
+					command.save(audioOutputPath + songTitle + '.mp3');
+					// console.log('\n\nwaiting7\n');
+                    // demo();
+                    // console.log('\n\nend waiting7\n');
+					// writeID3Tags(song, songTitle);
+					const time = Math.round((new Date() - start) / 1000);
+					console.log(`time: ${time} seconds! [${songTitle}]\n`);
+					if (tracks.length === 0) {
+						console.log('done!');
+						res.json({ye: 'yee'});
+					} else {
+						download()
+					}
+
+				});
+			}
+		}	
+	}
+    console.log('\n\nDownload beginning>>>');
+	download();
+}
+
+app.post('/api/label', function(req, res) {
+    label(req, res);
 });
 
-app.listen(port, function() {
-  console.log('\nServer is running on port ' + port + '.....\n')
-});
+function label(req, res) {
+    const tracks = req.body.tracks || [];
+    tracks.forEach((track) => {
+        const songTitle = track.label.replace(/[\/]/g, '|');
+        writeID3Tags(track, songTitle);
+	});
+    res.json({success: true})
+}
+
+
+
+function writeID3Tags(song, songTitle) {
+	const tags = {
+        artist: song.artist,
+        title: song.title,
+		album: song.album
+	};
+    console.log('song', song);
+	console.log('tags', tags);
+	const success = NodeID3.update(tags, audioOutputPath + songTitle + '.mp3');
+	console.log('success!', success);
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function demo() {
+    console.log('Taking a break...');
+    await sleep(7000);
+    console.log('Two second later');
+}
+
+
+
+app.listen(port, () => console.log(`Server started on port ${port}\n`));
--- public/app.js
+++ public/app.js
@@ -1,64 +1,56 @@
 'use strict';
 
-var currentPlaylist;
-var currentSingle;
+const app = angular.module('app', ['ui.router']);
 
-$('#get-single').click(function() {
-  $.ajax({
-    type: 'POST',
-    url: '/api/single',
-    data: {url: $('#url-single').val()},
-    success: function(response) {
-      console.log(response);
-      currentSingle = response;
-      document.getElementById('infobox-single').textContent = '';
-      var el = document.createElement('p');
-      el.textContent = response.title;
-      document.getElementById('infobox-single').appendChild(el);
+app.controller('mainCtrl', ['$state', function($state) {
+    $state.go('playlist');
+}]);
+
+app.controller('playlistCtrl', ['$scope', 'PlaylistService', function($scope, PlaylistService) {
+    $scope.tracks = [];
+    $scope.validUrl = false;
+
+    $scope.getPlaylistInfo = function(url) {
+        const delimiter = '?list=';
+        const uriIdx = url.indexOf(delimiter);
+        if (uriIdx > -1) {
+            const uri = url.slice(uriIdx + delimiter.length);
+            PlaylistService.getPlaylistInfo(uri).then(function(res) {
+                console.log('playlist response', res.data);
+                $scope.tracks = res.data;
+            });
+        } else {
+            console.error('Invalid playlist url');
+        }
     }
-  });
-});
 
-$('#download-single').click(function() {
-  $.ajax({
-    type: 'POST',
-    url: '/api/download',
-    data: {url: 'https://youtube.com' + currentSingle.url},
-    success: function(response) {
-      alert(response);
+    $scope.showId3Data = function() {
+        console.log($scope.tracks);
+    };
+
+    $scope.download = function() {
+        PlaylistService.downloadPlaylist($scope.tracks);
     }
-  });
-});
 
-$('#get-playlist').click(function() {
-  $.ajax({
-    type: 'POST',
-    url: '/api/playlist',
-    data: {url: $('#url-playlist').val()},
-    success: function(response) {
-      console.log(response);
-      currentPlaylist = response;
-      document.getElementById('infobox-playlist').textContent = '';
-      for (var i = 0; i < response.length; i++) {
-        var el = document.createElement('p');
-        el.textContent = response[i].title;
-        document.getElementById('infobox-playlist').appendChild(el);
-      }
+    $scope.writeData = function() {
+        PlaylistService.writeData($scope.tracks);
     }
-  })
+}]);
+
+app.config(function($stateProvider, $locationProvider) {
+    $locationProvider.hashPrefix('');
+    $stateProvider
+        .state('playlist', {
+            url: '/',
+            templateUrl: './playlistTemplate.html',
+            controller: 'playlistCtrl'
+        })
 });
 
-$('#download-playlist').click(function() {
-  for (var i = 0; i < currentPlaylist.length; i++) {
-    $.ajax({
-      type: 'POST',
-      url: '/api/download',
-      data: {url: 'https://youtube.com' + currentPlaylist[i].url},
-      async: false,
-      success: function(response) {
-        console.log(response);
-      }
-    });
-  }
-  alert('Playlist finished downloading!');
-});
+app.service('PlaylistService', ['$http', function($http) {
+    return {
+        getPlaylistInfo: (uri) => $http.get('/api/playlist/' + uri),
+        downloadPlaylist: (tracks) => $http.post('/api/download', {tracks: tracks}),
+        writeData: (tracks) => $http.post('/api/label', {tracks: tracks})
+    }
+}]);
